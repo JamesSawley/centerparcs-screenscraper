499ce6d24d75d1f4fbd195b4a31d1dfa16316a3c
James Sawley <james.r.sawley@bath.edu>
2017-11-04 23:58:03 +0000

    Loads page and finds first radio button

--- index.js
+++ index.js
@@ -1,2 +1,17 @@
-console.log("Hello, world!");
-phantom.exit();
+var page = require("webpage").create();
+page.open("http://reservations.centerparcs.co.uk/", function(status) {
+	if(status !== "success") {
+    console.log("Page load failed. Exiting phantomjs");
+    phantom.exit();
+	} else {
+    console.log("Page loaded: " + page.title);
+
+    var location = page.evaluate(function() {
+      return document.getElementById("woburnForest");
+    });
+    console.log("location: " + location.value);
+  }
+  setTimeout(function() {
+    phantom.exit();
+  }, 5000);
+});
