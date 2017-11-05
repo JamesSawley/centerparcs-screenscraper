eac30742818e345de1a47e34523bf6feba8f6408
James Sawley <james.r.sawley@bath.edu>
2017-11-05 00:47:22 +0000

    click woburn forest

--- index.js
+++ index.js
@@ -1,17 +1,31 @@
 var page = require("webpage").create();
 page.open("http://reservations.centerparcs.co.uk/", function(status) {
-	if(status !== "success") {
+
+  page.onConsoleMessage = function(msg) {
+    console.log(msg);
+  }
+
+  if(status !== "success") {
     console.log("Page load failed. Exiting phantomjs");
     phantom.exit();
 	} else {
     console.log("Page loaded: " + page.title);
 
-    var location = page.evaluate(function() {
-      return document.getElementById("woburnForest");
+    page.evaluate(function() {
+      console.log('Getting location... ')
+      var location = document.getElementById("woburnForest");
+
+      console.log("Clicking " + location.value);
+      location.click();
+
+      if(location.checked) {
+        console.log(location.value + " checked");
+      }
     });
-    console.log("location: " + location.value);
   }
+
   setTimeout(function() {
+    console.log("Timed out. Exiting phantomjs")
     phantom.exit();
   }, 5000);
 });
