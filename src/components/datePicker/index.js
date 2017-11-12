46d56240f2ea4804e5952b2407ce51f4b286faef
James Sawley <james.r.sawley@bath.edu>
2017-11-12 17:12:12 +0000

    Linter kicked in

--- src/components/datePicker/index.js
+++ src/components/datePicker/index.js
@@ -1,34 +1,36 @@
 import { h, Component } from 'preact';
-import style from './style';
+// import style from './style';
 
 export default class DatePicker extends Component {
-	constructor(props) {
-		super(props);
+	componentDidUnmount() {
+		this.base.removeChild(this.picker);
 	}
-  shouldComponentUpdate() {
-    return false;
-  }
-  componentWillReceiveProps(nextProps) {
-    if (this.picker && nextProps.value!==this.props.value) {
-      this.picker.value = nextProps.value;
-    }
-  }
-	componentDidMount() {
-    this.picker = document.createElement('input');
-    this.picker.type = 'date';
-    this.base.appendChild(this.picker);
 
-    // further example: events
-    this.picker.oninput = () => {
-      if (this.props.onChange) {
-        this.props.onChange({ value: this.picker.value });
-      }
-    };
-  }
-  componentDidUnmount() {
-    this.base.removeChild(this.picker);
-  }
-  render() {
-    return <div class="date-picker" />;
-  }
+	componentDidMount() {
+		this.picker = document.createElement('input');
+		this.picker.type = 'date';
+		this.base.appendChild(this.picker);
+
+		// further example: events
+		this.picker.oninput = () => {
+			if (this.props.onChange) {
+				this.props.onChange({ value: this.picker.value });
+			}
+		};
+	}
+
+	componentWillReceiveProps(nextProps) {
+		if (this.picker && nextProps.value!==this.props.value) {
+			this.picker.value = nextProps.value;
+		}
+	}
+
+	shouldComponentUpdate() {
+		return false;
+	}
+
+
+	render() {
+		return <div class="date-picker" />;
+	}
 }
--- src/routes/home/index.js
+++ src/routes/home/index.js
@@ -6,72 +6,34 @@ import style from './style';
 import config from '../../config.json';
 
 export default class Home extends Component {
-	constructor(){
-		super()
-		this.state = {
-			...config,
-			form: {}
+	handleInputChange(event) {
+		const target = event.target;
+		if (target === undefined) {
+			const date = moment(event.value).format('DD MM YYYY');
+			const day = moment(event.value).format('DD');
+			const month = moment(event.value).format('MM YYYY');
+			this.setState({ form: {
+				...this.state.form,
+				startDate: date,
+				month: month,
+				day: day
+			} });
 		}
-		console.log('state', this.state)
-		this.pricePaid = this.pricePaid.bind(this);
-		this.location = this.location.bind(this);
-		this.duration = this.duration.bind(this);
-		this.startDate = this.startDate.bind(this);
-		this.numAdults = this.numAdults.bind(this);
-		this.handleSubmit = this.handleSubmit.bind(this);
+		else {
+			const value = target.value;
+			const name = target.name;
+			this.setState({ form: {
+				...this.state.form,
+				[name]: value
+			} });
+		}
 	}
 
-	componentDidMount(){
-		console.log('state', this.state);
-	}
-
-	componentDidUpdate() {
-		console.log('updated state', this.state)
-	}
-
-	pricePaid(event) {
-		console.log(event.target.value);
-		this.setState({form: {
-			...this.state.form,
-			pricePaid: event.target.value
-		}});
-	}
-	location(event) {
-		console.log(event.target.value)
-		this.setState({form: {
-			...this.state.form,
-			location: event.target.value
-		}});
-	}
-	duration(event) {
-		this.setState({form: {
-			...this.state.form,
-			duration: event.target.value
-		}});
-	}
-	startDate(event) {
-		console.log(event.value)
-		const date = moment(event.value).format('DD MM YYYY');
-		const day = moment(event.value).format('DD');
-		const month = moment(event.value).format('MM YYYY');
-		this.setState({form: {
-			...this.state.form,
-			startDate: date,
-			month: month,
-			day: day
-		}});
-		console.log(this.state)
-	}
-	numAdults(event) {
-		this.setState({form: {
-			...this.state.form,
-			numAdults: event.target.value
-		}});
-	}
-  handleSubmit(event) {
+	handleSubmit(event) {
 		event.preventDefault();
-    this.setState({
+		this.setState({
 			...this.state,
+			form: {},
 			pricePaid: this.state.form.pricePaid,
 			location: this.state.form.location,
 			duration: this.state.form.duration,
@@ -79,53 +41,63 @@ export default class Home extends Component {
 			month: this.state.form.month,
 			day: this.state.form.day,
 			numAdults: this.state.form.numAdults
-		})
-  }
+		});
+	}
+
+	constructor(){
+		super();
+		this.state = {
+			...config,
+			form: {}
+		};
+		this.handleInputChange = this.handleInputChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
 
 	render() {
 		return (
 			<div class={style.home}>
 				<CurrentConfig
-					currentPrice = { this.state.currentPrice }
-					pricePaid = { this.state.pricePaid }
-					location = { this.state.location }
-					duration = { this.state.duration }
-					day = { this.state.day }
-					month = { this.state.month }
-					numAdults = { this.state.numAdults }
-					/>
+					currentPrice={this.state.currentPrice}
+					pricePaid={this.state.pricePaid}
+					location={this.state.location}
+					duration={this.state.duration}
+					day={this.state.day}
+					month={this.state.month}
+					numAdults={this.state.numAdults}
+				/>
 				<div>
 					<h2>Edit configuration</h2>
 					<form onSubmit={this.handleSubmit}>
 					  <label>
 					    Price paid:
-					    <input type="number" name="pricePaid" onChange={this.pricePaid} />
+					    <input type="number" name="pricePaid" onChange={this.handleInputChange} />
 					  </label>
 						<label>
 					    Location:
-							<select value={this.state.form.location} onChange={this.location}>
+							<select value={this.state.form.location} name="location" onChange={this.handleInputChange}>
 		            <option value="whinfellForest">Whinfell Forest, Cumbria</option>
 		            <option value="sherwoodForest">Sherwood Forest, Nottinghamshire</option>
 		            <option value="elvedenForest">Elveden Forest, Suffolk</option>
 		            <option value="woburnForest">Woburn Forest, Bedfordshire</option>
 								<option value="longleatForest">Longleat Forest, Wiltshire</option>
-	           	</select>
+							</select>
 					  </label>
 						<label>
 					    Duration:
-							<select value={this.state.form.duration} onChange={this.duration}>
+							<select value={this.state.form.duration} name="duration" onChange={this.handleInputChange}>
 		            <option value="midweekID">Mid week</option>
 		            <option value="weekendID">Weekend</option>
 		            <option value="1weekID">1 week</option>
-	           	</select>
+	            </select>
 					  </label>
 						<label>
 					    Start Date:
-							<DatePicker value={this.state.form.location} onChange={this.startDate} />
+							<DatePicker value={this.state.form.location} onChange={this.handleInputChange} />
 					  </label>
 						<label>
 					    Number of Adults:
-					    <input type="number" name="numAdults" value={this.state.form.numAdults} onChange={this.numAdults} />
+					    <input type="number" name="numAdults" value={this.state.form.numAdults} onChange={this.handleInputChange} />
 					  </label>
 					  <input type="submit" value="Submit" />
 					</form>
