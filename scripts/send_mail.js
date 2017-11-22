a8afde24b50b0385e313581a617fe62318e90dcd
James Sawley <james.r.sawley@bath.edu>
2017-11-22 17:01:35 +0000

    Modified to use config

--- scripts/send_mail.js
+++ scripts/send_mail.js
@@ -1,7 +1,10 @@
-var fs = require('file-system');
-var googleAuth = require('google-auth-library');
-var google = require('googleapis');
-var config = require('../config.json');
+const fs = require('file-system');
+const googleAuth = require('google-auth-library');
+const google = require('googleapis');
+const json = require('../config.json');
+
+const config = json.config;
+const mail = json.mail;
 
 function getOAuth2Client(cb) {
   // Load client secrets
@@ -9,12 +12,12 @@ function getOAuth2Client(cb) {
     if (err) {
       return cb(err);
     }
-    var credentials = JSON.parse(data);
-    var clientSecret = credentials.installed.client_secret;
-    var clientId = credentials.installed.client_id;
-    var redirectUrl = credentials.installed.redirect_uris[0];
-    var auth = new googleAuth();
-    var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
+    const credentials = JSON.parse(data);
+    const clientSecret = credentials.installed.client_secret;
+    const clientId = credentials.installed.client_id;
+    const redirectUrl = credentials.installed.redirect_uris[0];
+    const auth = new googleAuth();
+    const oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
     // Load credentials
     fs.readFile('gmail-credentials.json', function(err, token) {
@@ -29,25 +32,25 @@ function getOAuth2Client(cb) {
 }
 
 function sendSampleMail(auth, cb) {
-  var gmailClass = google.gmail('v1');
+  const gmailClass = google.gmail('v1');
 
-  var email_lines = [];
+  const email_lines = [];
 
 
-  email_lines.push('From: ' + config.mail.from);
-  email_lines.push('To: ' + config.mail.to);
-  email_lines.push('Content-type: ' + config.mail.contentType);
-  email_lines.push('MIME-Version: ' + config.mail.mimeVersion);
-  email_lines.push('Subject: ' + config.mail.subject);
+  email_lines.push('From: ' + mail.from);
+  email_lines.push('To: ' + mail.to);
+  email_lines.push('Content-type: ' + mail.contentType);
+  email_lines.push('MIME-Version: ' + mail.mimeVersion);
+  email_lines.push('Subject: ' + mail.subject);
   email_lines.push('');
-  config.mail.body.forEach(function(string){
+  mail.body.forEach(function(string){
     email_lines.push(string);
   });
   email_lines.push(config.currentPrice);
 
-  var email = email_lines.join('\r\n').trim();
+  const email = email_lines.join('\r\n').trim();
 
-  var base64EncodedEmail = new Buffer(email).toString('base64');
+  let base64EncodedEmail = new Buffer(email).toString('base64');
   base64EncodedEmail = base64EncodedEmail.replace(/\+/g, '-').replace(/\//g, '_');
 
   gmailClass.users.messages.send({
@@ -59,6 +62,7 @@ function sendSampleMail(auth, cb) {
   }, cb);
 }
 
+console.log('-------- Section 4 - Send email --------');
 if (config.currentPrice !== 0 && config.currentPrice !== undefined && config.currentPrice !== null) {
   if (config.currentPrice < config.pricePaid) {
     getOAuth2Client(function(err, oauth2Client) {
