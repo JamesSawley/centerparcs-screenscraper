a035821c7523604c685f58d8c91057bab4d0b1da
James Sawley <james.r.sawley@bath.edu>
2017-11-22 17:02:06 +0000

    Updated bash script

--- job.sh
+++ job.sh
@@ -1,11 +1,4 @@
 #!/bin/bash
 
-echo ----------------------------
-echo Job starting...
-echo
-echo Scraping Centerparcs website...
-node_modules/.bin/phantomjs scripts/scrape.js
+node scripts/scrape.js
 node scripts/send_mail.js
-echo ----------------------------
-echo job finished
-echo ----------------------------
