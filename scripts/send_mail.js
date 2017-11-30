5c77a198728fc10148c879cbcf1d4c10d39c4937
James Sawley <james.r.sawley@bath.edu>
2017-11-30 23:47:16 +0000

    Amended pricePaid

--- scripts/send_mail.js
+++ scripts/send_mail.js
@@ -1,20 +1,27 @@
 const sgMail = require('@sendgrid/mail');
-const price = require('../config.json').config.currentPrice;
+const config = require('../config.json');
+
+const currentPrice = config.currentPrice;
+const pricePaid = config.currentPrice;
 
 const sendMail = () => {
 	console.log('');
 	console.log('-------- Section 4 - Send email --------');
-
-	sgMail.setApiKey(process.env.SENDGRID_API_KEY);
-	const msg = {
-	  to: 'sawleyjr09@gmail.com',
-	  from: 'cheaper@centerparcs.com',
-	  subject: 'Discount available!',
-	  text: 'Call Center Parcs bookings on 03448 267723 now to secure the discount.',
-	  html: `<p>Call Center Parcs bookings on 03448 267723 now to secure the discount. You could now pay just £<strong>${price}</strong></p>`,
-	};
-	sgMail.send(msg)
-		.then(console.log('... mail sent'))
+	if (currentPrice < pricePaid ) {
+		sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+		const msg = {
+		  to: 'sawleyjr09@gmail.com',
+		  from: 'cheaper@centerparcs.com',
+		  subject: 'Discount available!',
+		  text: 'Call Center Parcs bookings on 03448 267723 now to secure the discount.',
+		  html: `<p>Call Center Parcs bookings on 03448 267723 now to secure the discount. You could now pay just £<strong>${price}</strong></p>`,
+		};
+		sgMail.send(msg)
+			.then(console.log('... mail sent'))
+	} else {
+		console.log('... price not cheaper. No mail sent.')
+	}
 }
+
 
 module.exports = sendMail;
